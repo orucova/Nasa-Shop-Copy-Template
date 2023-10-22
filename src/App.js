7f4fe9597ca20360eae75bdb910dd10bdedcf3ae
orucova <orucovazehra40@gmail.com>
2023-10-22 17:05:23 +0400

    creating

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+//Pages
+import Home from "./pages/Home"
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <>
+    <Header/>
+    <Routes>
+      <Route path='/' element={<Home/>}  />
+      </Routes>
+      <Footer/>
     </>
   )
 }
