651734ed268a7f9f9f34d2bd3ad11ba7176553fb
orucova <orucovazehra40@gmail.com>
2023-10-25 12:02:18 +0400

    wrote a part of header

---
+++ src/components/Dropdown.jsx
@@ -0,0 +1,66 @@
+import { NavLink } from "react-router-dom";
+const Dropdown = () => {
+  return (
+        <div className="row">
+
+      <ul className="shopDropdownList">
+        <NavLink to="/">
+          <li className="shopDropdownItem">Man</li>
+        </NavLink>
+        <NavLink to="/">
+          <li className="shopDropdownItem">Man</li>
+        </NavLink>
+        <NavLink to="/">
+          <li className="shopDropdownItem">Man</li>
+        </NavLink>
+        <NavLink to="/">
+          <li className="shopDropdownItem">Man</li>
+        </NavLink>
+      </ul>
+      <ul className="shopDropdownList">
+        <NavLink to="/">
+          <li className="shopDropdownItem">Man</li>
+        </NavLink>
+        <NavLink to="/">
+          <li className="shopDropdownItem">Man</li>
+        </NavLink>
+        <NavLink to="/">
+          <li className="shopDropdownItem">Man</li>
+        </NavLink>
+        <NavLink to="/">
+          <li className="shopDropdownItem">Man</li>
+        </NavLink>
+      </ul>
+      <ul className="shopDropdownList">
+        <NavLink to="/">
+          <li className="shopDropdownItem">Man</li>
+        </NavLink>
+        <NavLink to="/">
+          <li className="shopDropdownItem">Man</li>
+        </NavLink>
+        <NavLink to="/">
+          <li className="shopDropdownItem">Man</li>
+        </NavLink>
+        <NavLink to="/">
+          <li className="shopDropdownItem">Man</li>
+        </NavLink>
+      </ul>
+      <ul className="shopDropdownList">
+        <NavLink to="/">
+          <li className="shopDropdownItem">Man</li>
+        </NavLink>
+        <NavLink to="/">
+          <li className="shopDropdownItem">Man</li>
+        </NavLink>
+        <NavLink to="/">
+          <li className="shopDropdownItem">Man</li>
+        </NavLink>
+        <NavLink to="/">
+          <li className="shopDropdownItem">Man</li>
+        </NavLink>
+      </ul>
+        </div>
+  );
+};
+
+export default Dropdown;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
-
+import { NavLink, Link } from "react-router-dom";
+import logo from "../assets/images/logo.webp";
+//Icons
+import { FiSearch } from "react-icons/fi";
+import { GiAstronautHelmet } from "react-icons/gi";
+import { RiShoppingBag2Line } from "react-icons/ri";
+import Dropdown from "./Dropdown";
 const Header = () => {
   return (
-    <header>Header</header>
-  )
-}
+    <header className="header">
+      <div className="container">
+        <div className="row">
+          <div className="logo">
+            <Link to="/">
+              <img src={logo} alt="logo" />
+            </Link>
+          </div>
+          <nav className="navBar">
+            <ul className="navList">
+              <li className="navItem">
+                <NavLink className="a">
+                Shop
+                   </NavLink>
+                <div className="dropdownMenu">
+                  <Dropdown />
+                </div>
+              </li>
+                <li className="navItem">
+              <NavLink className="a" to="/collection">
+                  Collections
+              </NavLink>
+                  </li>
+                <li className="navItem">
+              <NavLink className="a" to="/nasaprograms">
+                  Nasaprograms
+              </NavLink>
+                </li>
+                <li className="navItem">
+              <NavLink className="a" to="/clearance">
+                  Clearance
+              </NavLink>
+                  </li>
+            </ul>
+          </nav>
+          <ul className="userArea">
+            <li className="icon search">
+              <FiSearch />
+            </li>
+            <li className="icon user">
+              <GiAstronautHelmet />
+            </li>
+            <li className="icon cartIcon">
+              <RiShoppingBag2Line />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
